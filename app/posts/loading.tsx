91b34eb932eb62aa5bbcd8fb3fdd8a7980f72fe4
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-09-19 11:31:41 +0600

    add a loading page for posts

---
+++ app/posts/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsLoading = () => {
+    return (
+        <div>
+            <span className="loading loading-spinner text-error"></span>
+        </div>
+    );
+};
+
+export default PostsLoading;
